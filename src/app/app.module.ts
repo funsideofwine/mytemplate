0d42ccb05fe8b23af769f94a18af6a6332664a44
Rolly Falco Villacacan <funsideofwine@gmail.com>
2019-04-20 12:18:55 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+import { PumpComponent } from './pump/pump.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PumpComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pump/pump.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pump',
+  templateUrl: './pump.component.html'
+})
+export class PumpComponent {
+
+}
